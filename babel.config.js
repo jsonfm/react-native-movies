9cb9f470d63f1c63564c25548be767d846018092
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2023-07-09 18:12:28 -0500

    initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,21 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      require.resolve("expo-router/babel"),
-    ],
-  };
+    api.cache(true);
+    return {
+        presets: ["babel-preset-expo"],
+        plugins: [
+            require.resolve("expo-router/babel"),
+            require.resolve("nativewind/babel"),
+            [
+                "module-resolver",
+                {
+                    root: ["."],
+                    extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+                    alias: {
+                        "@/components": "./components",
+                        "@/app": "./app",
+                    },
+                },
+            ],
+        ],
+    };
 };
